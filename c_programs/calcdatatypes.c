17736ea3443ecadd0530436f8b1081b23ce21207
robingreig <robin.greig@calalta.com>
2014-12-29 09:18:55 -0700

    added c_programs folder

---
+++ c_programs/calcdatatypes.c
@@ -0,0 +1,18 @@
+// This file will perform various calculations with 2 varialbles
+// RLG 2014.12.29
+
+#include <stdio.h>
+
+int main(){
+	float a=3;
+	float b=4;
+	float c=a+b;
+	float product=a*b;
+	float divisor=a/b;
+	printf("a + b = %.02f\n", c);
+	printf("a + b = %.02f\n", a+b);
+	printf("a * b = %.02f\n", product);
+	printf("a / b = %.02f\n", divisor);
+
+	return 0;
+}
