dbca074a6a2b39ba52ff12c629dea685d4f0e2b0
robingreig <robin.greig@calalta.com>
2015-03-07 08:32:22 -0700

    added file in TemPi110

--- TemPi110/BackupOfficeTemp.sh
+++ TemPi110/BackupOfficeTemp.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-mysqldump -u robin -pMicr0s0ft office_temp | gzip > /home/robin/OfficeTempBackup/office_temp_backup.`date +%F.%H:%M`.tempi110.sql.gz
-#scp -q /home/robin/OfficeTempBackup/office_temp-backup.`date +%F.%H-%M`.tempi110.sql.gz robin@50.93.51.6:/home/robin/OfficeTempBackup/
-scp /home/robin/OfficeTempBackup/`ls -tp /home/robin/OfficeTempBackup/ | grep -v /$ | head -1` robin@raspi15.hopto.org:/home/robin/office_temp_backup/
-scp /home/robin/OfficeTempBackup/`ls -tp /home/robin/OfficeTempBackup/ | grep -v /$ | head -1` robin@192.168.137.95:/home/robin/OfficeTempBackup/
-
+mysqldump -u robin -pMicr0s0ft office_temp | gzip > /home/robin/OfficeTempBackup/office_temp-backup.`date +%F.%H-%M`.tempi110.sql.gz
+scp -q /home/robin/OfficeTempBackup/office_temp-backup.`date +%F.%H-%M`.tempi110.sql.gz robin@raspi15.hopto.org:/home/robin/OfficeTempBackup/
+scp -q /home/robin/OfficeTempBackup/office_temp-backup.`date +%F.%H-%M`.tempi110.sql.gz robin@192.168.137.245:/home/robin/OfficeTempBackup/
+scp -q /home/robin/uptime.txt robin@raspi15.hopto.org:/home/robin/TemPi110.uptime.txt
 
