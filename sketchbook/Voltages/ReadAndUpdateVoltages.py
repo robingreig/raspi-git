1787e638384756825bac29389386f3560adc3a9c
robingreig <robin.greig@calalta.com>
2015-09-14 08:20:10 -0600

    updated ReadAndUpdateVoltage.py with comments

--- sketchbook/Voltages/ReadAndUpdateVoltages.py
+++ sketchbook/Voltages/ReadAndUpdateVoltages.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-
+#====================================================================
+# Import subfiles
+#====================================================================
 import datetime
 import MySQLdb
 import serial
@@ -8,35 +10,48 @@ import sys
 import time
 import warnings
 
+#====================================================================
 # Set Variables
+#====================================================================
 count = 0
 delay = 2
 DEBUG = 0
-maxcount = 2
+maxcount = 1
 
+#====================================================================
+# Setup Serial Port
+# /tty/AMA0 is used with the GertDuino board
+#====================================================================
 #ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
 #ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=5)
 #ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=5)
 ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=5)
 
+#====================================================================
+# Send the Analogue Port number to Gertduino & read back the voltage value
+#====================================================================
+
 #while True:
 while (count < maxcount):
   ser.flushInput()
-
   print "Count= {0}".format(count)
-  #ok read all Analog
+#   OK read all Analog
   for i in range(6):
     if DEBUG == 1:
 	print "\ti={0}".format(i)
+#   write the Analogue Port number to GertDuino
     ser.write(chr(48+i))
+#   read the response back from GertDuino
     line = ser.readline()
 #    print "\t{0}".format(line),
     print "\tA{0}=".format(i),
     print "{0}".format(line),
+#   If reading Analogue 0 then save the voltage to /home/robin/ReadVoltage0
     if i == 0:
       cht = open("/home/robin/ReadVoltage0", "wb")
       cht.write(line);
       cht.close()
+#     If the voltage is low on Analogue 0 then print "Low", or "Critical", or shutdown Raspi
       if float(line) < 12.00 and float(line) > 11.51:
         print "\t\tVoltage is low"
       elif float(line) < 11.50 and float(line) > 11.01:
@@ -47,9 +62,9 @@ while (count < maxcount):
   count = count +1
   time.sleep(delay)
 
-# ===========================================================================
+# ====================================================================
 # Open Database Connection
-# ===========================================================================
+# ====================================================================
 
 # Hostname, User, Password, Database
 dbc = MySQLdb.connect(host= "192.168.200.15",
@@ -60,16 +75,16 @@ dbc = MySQLdb.connect(host= "192.168.200.15",
 # Prepare a cursor
 cursor = dbc.cursor()
 
-# ==========================================================================
+# ====================================================================
 # Filter Warnings
-# ==========================================================================
+# ====================================================================
 
 warnings.filterwarnings('ignore', category=MySQLdb.Warning)
 
 
-# ==========================================================================
+# ====================================================================
 # Insert into table - variable
-# ==========================================================================
+# ====================================================================
 
 # Add a delay for boot
 time.sleep(delay)
