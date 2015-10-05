8fa729337d9378807632e1645c20c703b63a1bfb
robingreig <robin.greig@calalta.com>
2015-10-04 20:44:56 -0600

    updated Uptime in APRX

--- aprx/Uptime.sh
+++ aprx/Uptime.sh
@@ -5,5 +5,5 @@ date > ./uptime.txt
 uptime >> ./uptime.txt
 head -4 ./uptime.txt.tmp >> ./uptime.txt
 
-scp ./uptime.txt robin@192.168.200.15:/home/robin/APRX1.uptime.txt
+scp ./uptime.txt robin@raspi15.local:/home/robin/APRX1.uptime.txt
 
