a7d4c4a9e9fbccd1da64c3651b784e215f50a6b6
Robin Greig <robin.greig@calalta.com>
2015-11-04 09:37:02 -0700

    added BatteryShutdown.py

---
+++ Pi200/Uptime.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cp ./uptime.txt ./uptime.txt.tmp
+echo > ./uptime.txt
+date >> ./uptime.txt
+uptime >> ./uptime.txt
+head -6 ./uptime.txt.tmp >> ./uptime.txt
+
+#scp /home/robin/uptime.txt robin@raspi15.hopto.org:/home/robin/Pi200.uptime.txt
+scp /home/robin/uptime.txt robin@raspi15.local:/home/robin/Pi200.uptime.txt
+
