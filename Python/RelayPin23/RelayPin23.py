2a57c8127dd85d4717eb05588c03c3fb55f55a4e
robingreig <robin.greig@calalta.com>
2015-12-10 11:49:08 -0700

    updated RelayPin23.py

--- Python/RelayPin23/RelayPin23.py
+++ Python/RelayPin23/RelayPin23.py
@@ -6,12 +6,18 @@ import datetime
 import subprocess
 import sys
 import time
+import os
+import RPi.GPIO as GPIO
 import warnings
 
 #====================================================================
 # Set Variables
 #====================================================================
-DEBUG = 1
+DEBUG = 0
+pinNum = 23
+GPIO.setwarnings(False) # Don't display the warnings
+GPIO.setmode(GPIO.BCM) # Numbering scheme that corresponds to breakout board and pin layout
+GPIO.setup(pinNum,GPIO.OUT) # Rreplace pinNum with whatever pin you used, this sets up that pin as an output
 
 #====================================================================
 # Read back the voltage value & turn on the Battery Charger if necessary
@@ -19,7 +25,7 @@ DEBUG = 1
 
 while True:
 # Read the Battery Voltage from BattMon1
-  def read_CurrentAprxVoltage():
+  def read_CurrentBatteryVoltage():
     f = open("/home/robin/ReadVoltage4", "r")
     line1 = f.readlines()
     f.close
@@ -27,27 +33,43 @@ while True:
     volts = float(line2)
     return volts
 
-  BattVolts = (round(read_CurrentAprxVoltage(),2))
+  BattVolts = (round(read_CurrentBatteryVoltage(),2))
 
   if DEBUG == 1:
-    print "Battery Voltage: ", (round(read_CurrentAprxVoltage(),2))
-    print "Battery Voltage rounded: ", (round(read_CurrentAprxVoltage(),2))
+    print "Battery Voltage: ", (round(read_CurrentBatteryVoltage(),2))
+    print "\tBattVolts: ", BattVolts
 
 # If the voltage is low, then print "Low", or "Critical", or shutdown Raspi
   if BattVolts >= 28:
-    print "\t\tVoltage is > 28V"
+    print "\tBattVolts: ", BattVolts
+    print "\tVoltage is near the maximum level, & turning Battery Charger off"
+    GPIO.output(pinNum,GPIO.LOW) # Turn the Battery Charger off
   elif BattVolts < 28 and BattVolts >= 27:
-    print "\t\tVoltage is between 27V & 28V"
+    print "\tBattVolts: ", BattVolts
+    print "\tVoltage is Normal"
   elif BattVolts < 27 and BattVolts >= 26:
-    print "\t\tVoltage is between 26V & 27V"
+    print "\tBattVolts: ", BattVolts
+    print "\tVoltage is Normal"
   elif BattVolts < 26 and BattVolts >= 25:
-    print "\t\tVoltage is between 25V & 26V"
+    print "\tBattVolts: ", BattVolts
+    print "\tVoltage is getting Low"
   elif BattVolts < 25 and BattVolts >= 24:
-    print "\t\tVoltage is low (between 24V & 25V)"
-  elif BattVolts < 23.9 and BattVolts >= 23.5:
-    print "\t\tVoltage is low (between 23.5V & 23.9V)"
-  elif BattVolts < 23.5:
+    print "\tBattVolts: ", BattVolts
+    print "\tVoltage is getting Low"
+  elif BattVolts < 24 and BattVolts >= 23.5:
+    print "\tBattVolts: ", BattVolts
+    print "\tVoltage is getting too LOW & Battery Charger is turning on"
+    GPIO.output(pinNum,GPIO.HIGH) # Turn the Battery Charger on
+  elif BattVolts < 23.5 and BattVolts >= 23.0:
+    print "\tBattVolts: ", BattVolts
+    print "\tVoltage is CRITICAL & trying to turn Battery Charger on again"
+    GPIO.output(pinNum,GPIO.HIGH) # Turn the Battery Charger on
+  elif BattVolts < 23.0:
 #    subprocess.call(["sudo", "shutdown", "-r", "now"])
-    subprocess.call(["sudo", "shutdown", "-k", "now"])
-#    subprocess.call(["sudo", "poweroff"])
+#    subprocess.call(["sudo", "shutdown", "-k", "now"])
+    subprocess.call(["sudo", "poweroff"])
   break
+
+
+
+
