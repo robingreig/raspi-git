d92453009173445925df4c40d2671062988992ea
robingreig <robin.greig@calalta.com>
2015-12-20 19:24:26 -0700

    updated Raspi15 files

--- Raspi15/UpdateAprxTemps.sh
+++ Raspi15/UpdateAprxTemps.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-scp ./CurrentDeskTemp robin@raspi14.local:/home/robin/CurrentGarageTemp
-#scp ./CurrentHouseTemp robin@aprx3.local:/home/robin/CurrentHouseTemp
-scp ./CurrentOutsideTemp robin@raspi14.local:/home/robin/CurrentOutsideTemp
+#scp ./CurrentDeskTemp robin@raspi14.local:/home/robin/CurrentGarageTemp
+#scp ./CurrentOutsideTemp robin@raspi14.local:/home/robin/CurrentOutsideTemp
+
+scp ./CurrentDeskTemp robin@aprx3.local:/home/robin/CurrentGarageTemp
+scp ./CurrentHouseTemp robin@aprx3.local:/home/robin/CurrentHouseTemp
+scp ./CurrentOutsideTemp robin@aprx3.local:/home/robin/CurrentOutsideTemp
 
 
