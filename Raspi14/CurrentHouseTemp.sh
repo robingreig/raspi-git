c7226ac2ba8723f1f21b25d59e70c02dfa30a215
robingreig <robin.greig@calalta.com>
2015-12-20 19:13:24 -0700

    updated Raspi14 & Aprx python files

--- Raspi14/CurrentHouseTemp.sh
+++ Raspi14/CurrentHouseTemp.sh
@@ -1,7 +1,1 @@
-#scp /home/robin/CurrentHouseTemp robin@192.168.200.15:/home/robin/CurrentHouseTemp
 scp /home/robin/CurrentHouseTemp robin@raspi15.local:/home/robin/CurrentHouseTemp
-#scp /home/robin/beacon01.txt robin@aprx1.local:/home/robin/beacon01.txt
-#scp /home/robin/beacon01.txt robin@aprx2.local:/home/robin/beacon01.txt
-scp /home/robin/beacon01.txt robin@aprx3.local:/home/robin/beacon01.txt
-#scp /home/robin/beacon01.txt robin@192.168.200.24:/home/robin/beacon01.txt
-#scp /home/robin/house_stats_backup/
