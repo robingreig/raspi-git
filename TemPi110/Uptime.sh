6b639aaf59e06ba0a1190c112a3befc919b06b0e
robingreig <robin.greig@calalta.com>
2016-03-30 14:53:35 -0600

    updated Uptime.sh from TemPi110

--- TemPi110/Uptime.sh
+++ TemPi110/Uptime.sh
@@ -5,5 +5,9 @@ echo > ./uptime.txt
 date >> ./uptime.txt
 uptime >> ./uptime.txt
 head -6 ./uptime.txt.tmp >> ./uptime.txt
+echo >> ./uptime.txt
+ifconfig eth0 >> ./uptime.txt
+echo >> ./uptime.txt
+df -h >> ./uptime.txt
 
 scp -q /home/robin/uptime.txt robin@raspi15.hopto.org:/home/robin/TemPi110.uptime.txt
