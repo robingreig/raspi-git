7929b5f1bee1de7cacc1d65826161dd41eb9b52a
Robin Greig <robin.greig@calalta.com>
2016-04-24 17:56:12 -0600

    updated Raspi14

--- Raspi14/Uptime.sh
+++ Raspi14/Uptime.sh
@@ -5,4 +5,7 @@ echo > ./uptime.txt
 date >> ./uptime.txt
 uptime >> ./uptime.txt
 head -6 ./uptime.txt.tmp >> ./uptime.txt
+echo >> ./uptime.txt
+df -h >> ./uptime.txt
 
+scp /home/robin/uptime.txt robin@raspi15.local:~/Raspi14.uptime.txt
