b5736bdfca8886b7f4b6a49e8b0ce11f3321a633
robingreig <robin.greig@calalta.com>
2016-05-08 09:41:14 -0600

    cleaned up Gertduino02

--- Gertduino2/Uptime.sh
+++ Gertduino2/Uptime.sh
@@ -6,5 +6,6 @@ date >> ./uptime.txt
 uptime >> ./uptime.txt
 head -6 ./uptime.txt.tmp >> ./uptime.txt
 
+#scp ./uptime.txt raspi15.hopto.org:/home/robin/Gertduino2.uptime.txt
 scp ./uptime.txt raspi15.local:/home/robin/Gertduino2.uptime.txt
 
