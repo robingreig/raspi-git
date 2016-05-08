620d795e13b64962829e1a4f64ff683633a84698
Robin Greig <robin.greig@calalta.com>
2016-05-08 10:29:51 -0600

    updated Raspi20

--- Raspi20/Uptime.sh
+++ Raspi20/Uptime.sh
@@ -5,7 +5,7 @@ echo > ./uptime.txt
 date >> ./uptime.txt
 uptime >> ./uptime.txt
 head -6 ./uptime.txt.tmp >> ./uptime.txt
+echo >> ./uptime.txt
+df -h >> ./uptime.txt
 
-#scp /home/robin/uptime.txt robin@raspi15.hopto.org:/home/robin/Pi200.uptime.txt
-scp /home/robin/uptime.txt robin@raspi15.local:/home/robin/Pi200.uptime.txt
-
+scp /home/robin/uptime.txt robin@raspi15.local:~/Raspi20.uptime.txt
