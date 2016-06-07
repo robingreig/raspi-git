e5db5577d1b480d421dc6707b8e9988b8f3156fe
robingreig <robin.greig@calalta.com>
2016-06-07 17:59:56 -0600

    updated Raspi15

---
+++ Raspi15/CarefreeTemp.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#tail -n1 /home/robin/MapleHouseTemp.txt > /home/robin/CurrentMapleHouseTemp
+cp /home/robin/CurrentCarefreeTemp /home/robin/CarefreeTemp/CarefreeTemp.`date +%F-%H-%M`.txt
+
+
+
