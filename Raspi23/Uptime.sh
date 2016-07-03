d8e038736bae69b1d36fc05cadc7631fc04f9098
Robin Greig <robin.greig@calalta.com>
2016-07-02 22:12:28 -0600

    updated Raspi23

--- Raspi23/Uptime.sh
+++ Raspi23/Uptime.sh
@@ -6,6 +6,6 @@ date >> ./uptime.txt
 uptime >> ./uptime.txt
 head -6 ./uptime.txt.tmp >> ./uptime.txt
 
-scp /home/robin/uptime.txt robin@raspi15.hopto.org:/home/robin/Raspi23.uptime.txt
-#scp /home/robin/uptime.txt robin@raspi15.local:/home/robin/Pi201.uptime.txt
+#scp /home/robin/uptime.txt robin@raspi15.hopto.org:/home/robin/Raspi23.uptime.txt
+scp /home/robin/uptime.txt robin@raspi15.local:/home/robin/Raspi23.uptime.txt
 
