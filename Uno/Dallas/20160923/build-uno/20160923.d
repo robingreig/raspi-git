c45c78025d235c3dc064ee51ac516a618823153b
Robin Greig <robin.greig@calalta.com>
2016-09-23 15:56:17 -0600

    updated /Uno/Dallas/20160923

---
+++ Uno/Dallas/20160923/build-uno/20160923.d
@@ -0,0 +1,12 @@
+build-uno/20160923.o: 20160923.ino \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ OneWire.h DallasTemperature.h
