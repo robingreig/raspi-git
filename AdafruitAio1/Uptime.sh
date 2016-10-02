5922aa3f4bf3040877465dedbec50be68f219295
Robin Greig <robin.greig@calalta.com>
2016-10-01 20:51:47 -0600

    updated AdafruitAio1 Uptime

--- AdafruitAio1/Uptime.sh
+++ AdafruitAio1/Uptime.sh
@@ -7,8 +7,6 @@ uptime >> ./uptime.txt
 head -6 ./uptime.txt.tmp >> ./uptime.txt
 echo >> ./uptime.txt
 df -h >> ./uptime.txt
-echo >> ./uptime.txt
-ifconfig eth0 >> ./uptime.txt
 
-scp ./uptime.txt raspi15.local:/home/robin/Raspi14.uptime.txt
+scp ./uptime.txt raspi15.local:/home/robin/AdafruitAio1.uptime.txt
 
