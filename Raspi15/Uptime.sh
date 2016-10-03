b83d4014945db75c1366429bf525144466d5df62
robingreig <robin.greig@calalta.com>
2016-10-03 09:17:52 -0600

    updated Raspi15 Uptime

--- Raspi15/Uptime.sh
+++ Raspi15/Uptime.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 cp ./uptime.txt ./uptime.txt.tmp
 echo > ./uptime.txt
 date >> ./uptime.txt
 uptime >> ./uptime.txt
-head -6 ./uptime.txt.tmp >> ./uptime.txt
+head -4 ./uptime.txt.tmp >> ./uptime.txt
+echo >> ./uptime.txt
+df -h >> ./uptime.txt
+echo >> ./uptime.txt
+ifconfig eth0 >> ./uptime.txt
+
 
