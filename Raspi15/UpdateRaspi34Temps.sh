3c02f9f1589b92606fd25f0576e0de16daafffdb
robingreig <robin.greig@calalta.com>
2017-03-04 16:37:21 -0700

    updated Aio & Aprx temps update scripts

---
+++ Raspi15/UpdateRaspi34Temps.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+scp /home/robin/CurrentAtticTemp robin@raspi34.local:/home/robin/CurrentAtticTemp
+scp /home/robin/CurrentCarefreeTemp robin@raspi34.local:/home/robin/CurrentCarefreeTemp
+scp /home/robin/CurrentCeilingTemp robin@raspi34.local:/home/robin/CurrentCeilingTemp
+scp /home/robin/CurrentDeskTemp robin@raspi34.local:/home/robin/CurrentDeskTemp
+scp /home/robin/CurrentHouseTemp robin@raspi34.local:/home/robin/CurrentHouseTemp
+scp /home/robin/CurrentOfficeTemp robin@raspi34.local:/home/robin/CurrentOfficeTemp
+scp /home/robin/CurrentOutsideTemp robin@raspi34.local:/home/robin/CurrentOutsideTemp
+
+
