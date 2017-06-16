1d31ab0381018ead1454ea2f4797200323f19984
Robin <robin.greig@calalta.com>
2017-06-16 10:37:06 -0600

    updated alarm31

--- alarm31/alarm08.py
+++ alarm31/alarm08.py
@@ -3,14 +3,14 @@
 #-------------------------------------------------------------------
 # Name: alarm08.py
 # Author: Robin Greig
-# Date 2017.06.15
+# Date 2017.06.16
 # Check for input every 0.1 seconds.
 # Respond to an available input immediately, but do something else if idle.
 #-------------------------------------------------------------------
 import os, select, subprocess, sys, time
 import RPi.GPIO as GPIO
 
-# files monitored for input
+##### files monitored for input
 read_list = [sys.stdin]
 # select() should wait for this many seconds for input.
 # A smaller number means more cpu usage, but a greater one
@@ -18,49 +18,53 @@ read_list = [sys.stdin]
 # available and the program starting to work on it.
 timeout = 0.1 # seconds
 
-# Set time references
-exit_delay_time = time.time()
-last_work_time = time.time()
+##### Set time references
+# timer for exit or entry delay
+delay_time = time.time()
+# timer to check time for keyboard check
+last_code_time = time.time()
 
-# Set Variables
-delay_time_count = 30
+##### Set Variables
+alarm_time = 60 # how long for alarm to sound before reset
+armed_status = 0 # 0 = disarmed, 1 = armed
+correct_code = 0 # 0 = wrong code, 1 = correct code
+delay_time_count = 30 # amount of entry or exit time
+entry_delay = 0 # 0 = not started or running, 1 = completed
+exit_delay = 0 # 0 = not started or running, 1 = completed
+delay_time_count = 30 # exit or entry delay time
+PIR_sensor = 0 # 0 = no movement, 1 = movement
 
-# Armed Status 0 = Disarmed, 1 = Armed
-armed_status = 0
+##### For troubleshooting, set DEBUG to 1
+DEBUG = 1
 
-# For troubleshooting, set DEBUG to 1
-DEBUG = 0
-
-# Don't echo password to screen
+##### Don't echo password to screen
 os.system("stty -echo")
 
 if DEBUG > 0:
   print(time.time())
+  os.system("stty echo")
 
-# Set GPIO pins
-Alarm_LED = 22
+##### Set GPIO pins
+Alarm_LED = 24
 Armed_LED = 23
-Beeper = 24
-Exit_Delay_LED = 25
-PIR_Sensor = 26
-Ready_LED = 27
-Siren = 28
+Beeper = 20
+PIR_Sensor = 25
+Ready_LED = 18
+Siren = 21
 
-
-# Setup GPIO
+##### Setup GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM) #numbering scheme that matches Cobbler
 GPIO.setup(Alarm_LED,GPIO.OUT) # Set GPIO to output for Armed_LED
 GPIO.setup(Armed_LED,GPIO.OUT) # Set GPIO to output for Armed_LED
 GPIO.setup(Beeper,GPIO.OUT) # Set GPIO to output for Armed_LED
-GPIO.setup(Exit_Delay_LED,GPIO.OUT) # Set GPIO to output for Exit_Delay_LED
 GPIO.setup(PIR_Sensor,GPIO.IN) # Set GPIO to input for PIR Sensors
 GPIO.setup(Ready_LED,GPIO.OUT) # Set GPIO to output for Armed_LED
 GPIO.setup(Siren,GPIO.OUT) # Set GPIO to output for Armed_LED
 
 def treat_input(linein):
-  global last_work_time
-  global alarm_status
+  global last_code_time
+  global armed_status
   global Armed_LED
 #  print("Workin' it!", linein, end="")
   if DEBUG > 0:
@@ -69,26 +73,25 @@ def treat_input(linein):
     keyboard_press = int(linein)
     if DEBUG > 0:
       print('Is a number')
-    if int(linein)==4032572556:
+    if int(linein)==4038197350: # exit progrm
        os.system("stty echo")
        GPIO.output(Armed_LED, GPIO.LOW)
        sys.exit()
-    if int(linein)==4038197350:
+    if int(linein)==4032572556: # shutdown raspi
        os.system("stty echo")
        GPIO.output(Armed_LED, GPIO.LOW)
-#       sys.exit()
-       subprocess.call(["sudo", "shutdown", "-r", "now"])
-    if int(linein)==8980:
-      if alarm_status == 0:
-        alarm_status = 1
+       subprocess.call(["sudo", "shutdown", "-h", "now"])
+    if int(linein)==8980: # correct alarm code
+      if armed_status == 0:
+        armed_status = 1
         if DEBUG > 0:
-          print('Alarm Status = ',alarm_status)
+          print('Armed Status = ',armed_status)
         print('Alarm Armed')
         GPIO.output(Armed_LED, GPIO.HIGH)
       else:
         alarm_status = 0
         if DEBUG > 0:
-          print('Alarm Status = ',alarm_status)
+          print('Armed Status = ',armed_status)
         print('Alarm Disarmed')
         GPIO.output(Armed_LED, GPIO.LOW)
   except ValueError:
@@ -96,34 +99,34 @@ def treat_input(linein):
     if DEBUG > 0:
       print('Incorrect, try again')
   time.sleep(1) # working takes time
-  last_work_time = time.time()
+  last_code_time = time.time()
 
-def delay_timer():
-  global exit_delay_time
+def exit_delay():
+  global exit_delay
   global delay_time_count
-  global alarm_status
+  global armed_status
   now = time.time()
   # do some other stuff every second of idleness
-  if alarm_status > 0:
+  if armed_status > 0:
     GPIO.output(Exit_Delay_LED,GPIO.HIGH)
-  #  if now - last_work_time > 2:
+  #  if now - last_code_time > 2:
   if now - exit_delay_time > 1:
     if DEBUG > 0:
       print('Running Delay Timer function')
     exit_delay_time = now
 
 def monitor_PIR():
-  global last_work_time
-  global alarm_status
+  global last_code_time
+  global armed_status
   now = time.time()
   # do some other stuff every second of idleness
-  if alarm_status > 0:
+  if armed_status > 0:
     GPIO.output(Exit_Delay_LED,GPIO.HIGH)
-  #  if now - last_work_time > 2:
-  if now - last_work_time > 1:
+  #  if now - last_code_time > 2:
+  if now - last_code_time > 1:
     if DEBUG > 0:
-      print('her stuff.')
-    last_work_time = now
+      print('do other stuff.')
+    last_code_time = now
 
 def main_loop():
   global read_list
