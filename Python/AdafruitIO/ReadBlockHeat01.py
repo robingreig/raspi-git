fcd6624597e29a45e178d9c98eda7807c03d7d05
Robin <robin.greig@calalta.com>
2018-04-16 19:21:14 -0600

    updated AdafruitIO

---
+++ Python/AdafruitIO/ReadBlockHeat01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# Import Library & create instance of REST client
+from Adafruit_IO import Client
+aio = Client('7e01e8b5e56360efc48a27682324fc353e18d14f')
+
+# Retrieve the most recent value from 'Outside-Temp'
+data = aio.receive('BlockHeat01')
+print('Received Value: {0}'.format(data.value))
+
