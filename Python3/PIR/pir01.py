5c3318ae08c8430c0d13f372c50a80b714cc9341
Robin <robin.greig@calalta.com>
2018-10-13 16:51:56 -0600

    added PIR

---
+++ Python3/PIR/pir01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import time
+ 
+SENSOR_PIN = 23
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SENSOR_PIN, GPIO.IN)
+ 
+def my_callback(channel):
+    # Here, alternatively, an application / command etc. can be started.
+    print('There was a movement!')
+ 
+try:
+    GPIO.add_event_detect(SENSOR_PIN , GPIO.RISING, callback=my_callback)
+    while True:
+        time.sleep(100)
+except KeyboardInterrupt:
+    print "Finish..."
+GPIO.cleanup()
