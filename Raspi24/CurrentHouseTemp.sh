2fbb7c21f2854ec1fe9569e9d554954ff8260ba9
Robin Greig <robin.greig@calalta.com>
2018-11-07 12:29:20 -0700

    updated Raspi24

--- Raspi24/CurrentHouseTemp.sh
+++ Raspi24/CurrentHouseTemp.sh
@@ -1,1 +1,2 @@
-scp /home/robin/CurrentHouseTemp robin@raspi15.local:/home/robin/CurrentHouseTemp
+#scp /home/robin/CurrentHouseTemp robin@raspi15.local:/home/robin/CurrentHouseTemp
+scp /home/robin/CurrentHouseTemp robin@raspi34.local:/home/robin/CurrentHouseTemp
