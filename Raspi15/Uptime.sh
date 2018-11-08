b34c99c1ce21fcffef59c53466970f8e5250c926
Robin Greig <robin.greig@calalta.com>
2018-11-08 09:03:04 -0700

    updated Raspi15

--- Raspi15/Uptime.sh
+++ Raspi15/Uptime.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 cp ./uptime.txt ./uptime.txt.tmp
 echo > ./uptime.txt
@@ -10,4 +10,5 @@ df -h >> ./uptime.txt
 echo >> ./uptime.txt
 ifconfig eth0 >> ./uptime.txt
 
-
+#scp -P 81 -q /home/robin/uptime.txt robin@robin.webhop.me:/home/robin/Raspi15.uptime.txt
+scp /home/robin/uptime.txt robin@raspi34.local:/home/robin/Raspi15.uptime.txt
