0e7fa00e21a948b3e619ae4195fd56c856bb11b2
robin.greig@calalta.com <robin.greig@calalta.com>
2018-12-05 16:16:09 -0700

    updated Barcode-Arran

---
+++ Barcode/Barcode-Arran.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+import time
+import RPi.GPIO as gpio
+Debug = 0
+#time.sleep(4)
+list1 = ['000008329','Robin Greig','000785923','Arran Woodruff']
+try:
+    while True:
+        name = input("\033[5;30;47m\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n    MakerSpace Key Access System, Please scan or input your ID number (including leading zeros) \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\033[0;37;40m")
+        if Debug > 0:
+            print("Your ID number is: "+ name +"!")
+            time.sleep(5)
+        if name in list1:
+            test1 =(list1.index(name))
+            test2 = test1 + 1
+            print("\n\n\n\n\033[5;30;42m\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n                         "+list1[test2]+" is authorized to sign out the key \n\n\n\n\n\n\n\n\n\n\n\n\n\n          \033[0;37;40m")
+            time.sleep(5)
+        else:
+            print("\n\n\n\n\033[5;30;41m\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n                           "+name+" is NOT authorized to sign out the key \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n       \033[0;37;40m")
+            time.sleep(5)
+except KeyboardInterrupt:
+    sys.exit()
