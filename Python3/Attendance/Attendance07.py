7f2bf752067cf0a56dcebed94985604d1416272e
Robin Greig <robin.greig@calalta.com>
2018-12-10 12:15:57 -0700

    updated Attendance07

--- Python3/Attendance/Attendance07.py
+++ Python3/Attendance/Attendance07.py
@@ -11,9 +11,12 @@ debug = 1
 delay1 = 2
 # Time Delay for rejection messages
 delay2 = 3
-# Did they sign a waiver > 0?
+# Did they sign a waiver? waiverSign == 1
 waiverSign = 0
-# Did they already sign in today?
+# Did they sign in today?
+#  0 = Not signed in
+#  1 = Signed in but not signed out
+#  2 = Signed in and signed out
 signIn = 0
 
 while True:
@@ -21,12 +24,11 @@ while True:
     if debug == 0:
         os.system('clear')
     waiverSign = 0
-
+    signIn = 0
 #	 Scan SAIT Barcode
-    sait =  input ("\n\n\nPlease scan your SAIT ID: ")
+    sait =  int(input("\n\n\nPlease scan your SAIT ID: "))
     if debug > 0:
         print("\nYour SAIT ID: ",sait)
-    user = sait
 
 #	Check to see if they in the users list?
     try:
@@ -36,7 +38,8 @@ while True:
             print ("\nWelcome to the MakerSpace ""%s"%firstName + " %s"%lastName+"!")
             if debug > 0:
                 print("\nRowcount returned from checking if they are in the system: ", cursor.rowcount)
-                time.sleep(delay1)
+                print("\nsignIn value: ",signIn)
+                time.sleep(delay2)
             time.sleep(delay1)
             waiverSign = 1
         if waiverSign == 0:
@@ -47,68 +50,72 @@ while True:
     finally:
         cursor.close()
 
-#	Have they signed in earlier today? numberRows will indicate number of times
-    try:
-        cursor = mariadb_connection.cursor()
-        cursor.execute("SELECT * from attendance where date = curdate() and saitID = '%s'"%(user))
-        results = cursor.fetchall()
-        numberRows = cursor.rowcount
-        if debug > 0:
-            print("\nRowcount returned from checking entries today: ", numberRows, "\n")
-            time.sleep(delay1)
-            for row in results:
-                print(row)
-    except mariadb.Error as error:
-        print("\nError#2: {}".format(error))
-    finally:
-        cursor.close()
+#	Have they signed in earlier today, but not signed out? numberRows will indicate number of times
+    if waiverSign > 0:
+        try:
+            cursor = mariadb_connection.cursor()
+#            cursor.execute("SELECT * from attendance where date = curdate() and saitID = '%s'"%(sait))
+            cursor.execute("SELECT * from attendance where timeOUT IS NULL and date = curdate() and saitID = '%s'"%(sait))
+            results = cursor.fetchone()
+            numberRows = cursor.rowcount
+            if numberRows == 1:
+                signIn = 1
+            if debug > 0:
+                print("\nRowcount returned from checking entries today: ", numberRows, "\n")
+                print("\nIf number of rows < 1, SignIn value should be 0 > ",signIn,"\n")
+                time.sleep(delay2)
+                while results is not None:
+                    print("\nResults: ",results)
+                    results = cursor.fetchone()
+                time.sleep(delay2)
+        except mariadb.Error as error:
+            print("\nError#2: {}".format(error))
+        finally:
+            cursor.close()
 
-#	Odd number of rows then they must be checking out?
-    if ((numberRows%2==0) and debug > 0):
-        signIn = 0
-        print("\nScanned times are even")
-        print("\nsignIn: ",signIn)
-    else:
-        signIn = 1
-        print("\nScanned times are odd")
-        print("\nsignIn: ",signIn)
-
-#	Add check out time timeOUT to last entry for user
-    if waiverSign > 0 and signIn > 0:
+#	Add check out timeOUT to last entry for user
+    if waiverSign > 0 and signIn == 1:
 #       Select the record that has the timeIN entry
         try:
             cursor = mariadb_connection.cursor()
-            cursor.execute("SELECT attendID from attendance where date = curdate() and saitID = '%s'"%(user))
+            if debug > 0:
+                print("\nSelect the record with the timeIN entry")
+            cursor.execute("SELECT attendID from attendance where timeOUT IS NULL and date = curdate() and saitID = '%s'"%(sait))
             results = cursor.fetchone()
             numberRows = cursor.rowcount
             if debug > 0:
-                print("\nRowcount returned from checking entries today: ", numberRows, "\n")
+                print("\nRowcount from Select the record with the timeIN entry: ", numberRows, "\n")
                 time.sleep(delay1)
                 if results is not None:
-                    print("\nOdd number of entries, want to update login entry\n")
-                    print(results)
+                    print("\nEntry number found: ",results,"\n")
                     time.sleep(delay1)
         except mariadb.Error as error:
             print("\nError#2: {}".format(error))
         finally:
             cursor.close()
+
 #	Update the record that was selected previously
         try:
             cursor = mariadb_connection.cursor()
+            if debug > 0:
+                print("\nUpdate the record that was previously selected")
             cursor.execute("UPDATE attendance set timeOUT = NOW() where attendID = '%s'"%(results))
             mariadb_connection.commit()
+            signIn = 0
             if debug > 0:
-                print ("Database Updated")
-                print ("The last inserted ID was: ", cursor.lastrowid)
+                print ("\nDatabase Updated")
+                print ("\nThe last inserted ID was: ", cursor.lastrowid)
+                print ("\nSignIn value: ",signIn)
                 time.sleep(delay1)
         except mariadb.Error as error:
             print("\nError#2: {}".format(error))
             mariadb_connection.rollback()
         finally:
             cursor.close()
+            signIn = 2
 
 #	If they haven't signed in today, then add an entry to the database timeIN
-    if waiverSign > 0 and signIn < 1:
+    if waiverSign == 1 and signIn == 0:
         try:
             if debug > 0:
                 print("\nYour SAIT ID: ",sait)
@@ -123,10 +130,11 @@ while True:
                 print ("\nDatabase Updated")
                 print ("\nThe last inserted ID was: ", cursor.lastrowid)
                 time.sleep(delay1)
-            cursor.close()
         except mariadb.Error as error:
            print("Error: {}".format(error))
            mariadb_connection.rollback()
+        finally:
+            cursor.close()
 
 mariadb_connection.close()
 
