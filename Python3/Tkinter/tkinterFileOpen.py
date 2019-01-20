dbc79aaa12bec16578c473c45f946b32126f1c42
Robin Greig <robin.greig@calalta.com>
2019-01-19 20:59:51 -0700

    updated Tkinter

--- Python3/Tkinter/tkinterFileOpen.py
+++ Python3/Tkinter/tkinterFileOpen.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from tkinter import *
-import tkinter.filedialog
+#import tkinter.filedialog
+from tkinter import filedialog
 
 root = Tk()
-root.filename = filedialog.asksaveasfilename(initialdir = "/", title = "Select File"
+root.filename = filedialog.asksaveasfilename(initialdir = "/", title = "Select File", filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+print(root.filename)
 mainloop()
