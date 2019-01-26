dfad95f8e9f960ac88b37dc32eafdad31a9a9633
Robin Greig <robin.greig@calalta.com>
2019-01-26 07:55:26 -0700

    updated makerspace_backup

--- SAIT/makerspace_backup.sh
+++ SAIT/makerspace_backup.sh
@@ -26,6 +26,8 @@ mysqldump -u robin -pMicr0s0ft makerspace | gzip > /home/robin/mariadb/makerspac
 # Copy backup to raspi20 NOT Including hour & minute
 scp /home/robin/mariadb/makerspace_backup.`date +%F`.sql.gz robin@raspi20.hopto.org:/home/robin/mariadb/
 
+# Copy backup to SMStest NOT Including hour & minute
+scp /home/robin/mariadb/makerspace_backup.`date +%F`.sql.gz robin@10.248.128.1:/home/robin/mariadb/
 
 
 
