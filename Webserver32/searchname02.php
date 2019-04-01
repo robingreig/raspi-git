559a074ea9a6318ef9f9df6312d9ce4adcac2ded
Robin Greig <robin.greig@calalta.com>
2019-03-31 21:24:47 -0600

    updated Webserver

--- Webserver32/searchname02.php
+++ Webserver32/searchname02.php
@@ -86,9 +86,9 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     //Table starting tag and header cells
-    echo " <table style='width: 80%; text-align: left; margin-left: auto; margin-right: auto;' border='0' cellpadding='2' cellspacing='2'><tr><th>UserID</th><th>Name</th><th>SAIT ID</th><th>Phone</th><th>SAIT Email</th><th>School</th><th>Program</th></tr>";
+    echo " <table style='width: 90%; text-align: left; margin-left: auto; margin-right: auto;' border='0' cellpadding='2' cellspacing='2'><tr><th>UserID</th><th>Name</th><th>SAIT ID</th><th>Phone</th><th>SAIT Email</th><th>School</th><th>Program</th><th>Active</th><th>Waiver</th><th>Mentor</th><th>3dPrint</th><th>Router</th><th>Solder</th><th>Drillpress</th></tr>";
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row['userID'] . "</td><td>" . $row['firstName'] ." ". $row['lastName'] ."</td><td>". $row['saitID'] ."</td><td>". $row['phone'] ."</td><td>". $row['email1'] ."</td><td>". $row['school'] ."</td><td>". $row['program']. "<br>";
+        echo "<tr><td>" . $row['userID'] . "</td><td>" . $row['firstName'] ." ". $row['lastName'] ."</td><td>". $row['saitID'] ."</td><td>". $row['phone'] ."</td><td>". $row['email1'] ."</td><td>". $row['school'] ."</td><td>". $row['program'] ."</td><td>". $row['active'] ."</td><td>". $row['waiver'] ."</td><td>". $row['mentor'] ."</td><td>". $row['3dprint'] ."</td><td>". $row['router'] ."</td><td>". $row['solder'] ."</td><td>". $row['drillpress'] ."<br>";
 //        echo "id: " . $row["userID"]. " - Name: " . $row["firstName"]. " " . $row["lastName"]. " ". " - SAIT ID: ".$row["saitID"]."<br>";
     }
     echo "</table>";
