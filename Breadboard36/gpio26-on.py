5ffaf651c109d99f6f60f81653a2c6df354ec659
Robin Greig <robin.greig@calalta.com>
2019-04-13 17:19:41 -0600

    updated Breadboard36

---
+++ Breadboard36/gpio26-on.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import time
+import os
+import RPi.GPIO as GPIO
+
+pinNum = 26
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM) #numbering scheme that corresponds to breakout board and pin layout
+GPIO.setup(pinNum,GPIO.OUT) #replace pinNum with whatever pin you used, this sets up that pin as an output
+
+GPIO.output(pinNum,GPIO.HIGH)
+#GPIO.cleanup()
