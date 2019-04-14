d846501866b6a7dc1ca334e27bdd3803485840af
Robin Greig <robin.greig@calalta.com>
2019-04-14 09:40:51 -0600

    updated RaspiLED

--- RaspiLED/digital_out04.py
+++ RaspiLED/digital_out04.py
@@ -40,6 +40,7 @@ pinNum = 26
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM) #numbering scheme that corresponds to breakout board and pin layout
 GPIO.setup(pinNum,GPIO.OUT) #replace pinNum with whatever pin you used, this sets up that pin as$
+GPIO.output(pinNum,GPIO.LOW) #set GPIO Low for default
 
 while count < 5:
     data = aio.receive(digital.key)
