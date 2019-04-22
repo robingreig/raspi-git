0310af498565432f468426b2a8cb85fb76e571fe
Robin Greig <robin.greig@calalta.com>
2019-04-21 21:58:24 -0600

    updated pwm

--- Breadboard36/gpio26-pwm50.py
+++ Breadboard36/gpio26-pwm50.py
@@ -8,21 +8,27 @@
 # 2019.04.21
 
 import RPi.GPIO as GPIO
+import time
 
 pinNum = 26	# Pin number used to drive FET for LED's
-dc = 50		# Set dc variable to 0 for 0%
+dc = 0		# Set dc variable to 0 for 0% to start
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM) # Numbering scheme that corresponds to breakout board and pin layout
-GPIO.setup(pinNum,GPIO.OUT) # This sets up pinNum as an output
-pwm = GPIO.PWM(pinNum, 100) # Iintialize PWM on pinNum @ 100Hz frequency
+GPIO.setmode(GPIO.BCM) 		# Numbering scheme that corresponds to breakout board and pin layout
+GPIO.setup(pinNum,GPIO.OUT) 	# This sets up pinNum as an output
+pwm = GPIO.PWM(pinNum, 100) 	# Iintialize PWM on pinNum @ 100Hz frequency
 
 # main loop of program
-print("\nPress Ctrl C to quit \n")	# Print blank line before and after message
+print("\nPress Ctrl C to quit \n")
+pwm.start(dc)			# Start pwm @ 0%
 
 try:
-  while True:			# Loop unti Ctrl C is pressed to stop
-    pwm.start(dc)		# Start PWM with 50% duty cycle
+  while True:				# Loop unti Ctrl C is pressed to stop
+    if dc < 50:
+      for dc in range (0, 51, 10):	# Ramp up to 50% by 5
+        pwm.ChangeDutyCycle(dc)		# Ramp up to 50% duty cycle
+        time.sleep(0.1)			# Sleep 0.1 sec
+        print(dc)
 except KeyboardInterrupt:
     print("Ctrl C pressed - ending program")
 
