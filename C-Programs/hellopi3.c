8589fd949b831e097730207397a2d2fed0e6dbe2
Robin Greig <robin.greig@calalta.com>
2019-04-23 20:40:52 -0600

    updated C-Programs

---
+++ C-Programs/hellopi3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main (void)
+{
+  /* A print Statement */
+  printf ("Hello World!\n");
+}
---
+++ C-Programs/intvar02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main (void)
+{
+  float a;
+  int b = 3;
+  int c;
+  
+  a = 2;
+  c = a + b;
+  printf ("The sum of adding %.2f and %d is %d\n", a, b, c);
+}
