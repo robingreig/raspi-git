31bc75906722ecb2454ed672390ef00590517dcf
Robin Greig <robin.greig@calalta.com>
2019-08-10 14:25:38 -0600

    updated Python3

--- Python3/MORTHE20190812/BlankSelfRun.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#
-#  Title: BlankSelfRun.py
-#  Date: 2019.08.10
-#  Author: Robin Greig
-#  
-#  
-
--- Python3/MORTHE20190812/HelloWorld01.py
+++
@@ -1,7 +0,0 @@
-#
-#  Title: HelloWorld01.py
-#  Date: 2019.08.10
-#  Author: Robin Greig
-#  
-#  
-print ("Hello World!")
