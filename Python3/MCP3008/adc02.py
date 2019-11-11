279b0f1d6ef83fd6cdf4f66290cba038056ee3ad
Robin Greig <robin.greig@calalta.com>
2019-11-10 21:09:50 -0700

    updated MCP3008

--- Python3/MCP3008/adc02.py
+++ Python3/MCP3008/adc02.py
@@ -5,19 +5,18 @@ import spidev # To communicate with SPI devices
 from time import sleep
 import datetime
 
-# Variables
-DEBUG = 1
-
+# To add delay
 # Start SPI connection
 spi = spidev.SpiDev() # Created an object
-spi.open(0,0)
-
+spi.open(0,0) 
 # Read MCP3008 data
 def analogInput(channel):
   spi.max_speed_hz = 1350000
   adc = spi.xfer2([1,(8+channel)<<4,0])
   data = ((adc[1]&3) << 8) + adc[2]
   return data
+
+
 
 # Below function will convert data to voltage
 def Volts(data):
@@ -26,8 +25,11 @@ def Volts(data):
   return volts
  
 while True:
-  input0 = analogInput(0) # Reading from CH0
-  input0_volts = Volts(input0)
-  timestamp = datetime.datetime.now()
-  print("Input 0: {} ({} Bits) ({}V)".format(timestamp, input0,input0_volts))
+  temp_output = analogInput(1) # Reading from CH0
+  temp_volts = Volts(temp_output)                                                                                          
+  x = datetime.datetime.now()
+  print("vibration : {} {} ({}V)".format(temp_output,x,temp_volts))
+  if (temp_output > 200):
+    print ("The tap is ON")
+  
   sleep(5)
