5b451da68f5120861e03f9480db31c47a2a5a4af
Robin <robin.greig@calalta.com>
2020-05-26 20:06:18 -0600

    updated Battmon24

--- Battmon24/battmon24adc02.py
+++ Battmon24/battmon24adc02.py
@@ -4,12 +4,18 @@
 import spidev # To communicate with SPI devices
 from time import sleep
 import datetime
+import os
 
 # Variables
 DEBUG = 1
 sleepTime = 1
 repeat = 3
 input0voltsAvg = 0
+input1voltsAvg = 0
+input2voltsAvg = 0
+input3voltsAvg = 0
+input4voltsAvg = 0
+input5voltsAvg = 0
 
 # Start SPI connection
 spi = spidev.SpiDev() # Created an object
@@ -38,25 +44,68 @@ while repeat > 0:
   input0_volts = Volts24(input0)
   input0voltsAvg = input0voltsAvg + input0_volts # Average all 3 entries
   timestamp = datetime.datetime.now()
-#  print("\nInput 0: {} ({} Bits) ({}V)".format(timestamp, input0,input0_volts))
-  print("\nInput 0: {} ({} Bits) ({}V ({}V)".format(timestamp, input0,input0_volts,input0voltsAvg))
+  print("\nInput 0: {} ({} Bits) ({}V) ({}V)".format(timestamp, input0,input0_volts,input0voltsAvg))
   if repeat == 1:
     input0voltsAvg = input0voltsAvg / 3
-    print("\nInput 0: {} ({} Bits) ({}V ({}V)".format(timestamp, input0,input0_volts,input0voltsAvg))
+    input0voltsAvg = round(input0voltsAvg, 2) # Round off to 2 decimal places
+    print("\nInput 0: {} ({} Bits) ({}V) ({}V)".format(timestamp, input0,input0_volts,input0voltsAvg))
+    cht = open("/home/robin/CurrentAdc0Volts", "w")
+    cht.write (str(input0voltsAvg))
+    cht.close()
   input1 = analogInput(1) # Reading from CH1
   input1_volts = Volts24(input1)
-  print("Input 1: {} ({} Bits) ({}V)".format(timestamp, input1,input1_volts))
+  input1voltsAvg = input1voltsAvg + input1_volts # Average all 3 entries
+  print("Input 1: {} ({} Bits) ({}V) ({}V)".format(timestamp, input1,input1_volts,input1voltsAvg))
+  if repeat == 1:
+    input1voltsAvg = input1voltsAvg / 3
+    input1voltsAvg = round(input1voltsAvg, 2) # Round off to 2 decimal places
+    print("\nInput 1: {} ({} Bits) ({}V) ({}V)".format(timestamp, input1,input1_volts,input1voltsAvg))
+    cht = open("/home/robin/CurrentAdc1Volts", "w")
+    cht.write (str(input1voltsAvg))
+    cht.close()
   input2 = analogInput(2) # Reading from CH2
   input2_volts = Volts12(input2)
-  print("Input 2: {} ({} Bits) ({}V)".format(timestamp, input2,input2_volts))
+  input2voltsAvg = input2voltsAvg + input2_volts # Average all 3 entries
+  print("Input 2: {} ({} Bits) ({}V) ({}V)".format(timestamp, input2,input2_volts,input2voltsAvg))
+  if repeat == 1:
+    input2voltsAvg = input2voltsAvg / 3
+    input2voltsAvg = round(input2voltsAvg, 2) # Round off to 2 decimal places
+    print("\nInput 2: {} ({} Bits) ({}V) ({}V)".format(timestamp, input2,input2_volts,input2voltsAvg))
+    cht = open("/home/robin/CurrentAdc2Volts", "w")
+    cht.write (str(input2voltsAvg))
+    cht.close()
   input3 = analogInput(3) # Reading from CH3
   input3_volts = Volts12(input3)
-  print("Input 3: {} ({} Bits) ({}V)".format(timestamp, input3,input3_volts))
+  input3voltsAvg = input3voltsAvg + input3_volts # Average all 3 entries
+  print("Input 3: {} ({} Bits) ({}V) ({}V)".format(timestamp, input3,input3_volts,input3voltsAvg))
+  if repeat == 1:
+    input3voltsAvg = input3voltsAvg / 3
+    input3voltsAvg = round(input3voltsAvg, 2) # Round off to 2 decimal places
+    print("\nInput 3: {} ({} Bits) ({}V) ({}V)".format(timestamp, input3,input3_volts,input3voltsAvg))
+    cht = open("/home/robin/CurrentAdc3Volts", "w")
+    cht.write (str(input3voltsAvg))
+    cht.close()
   input4 = analogInput(4) # Reading from CH4
   input4_volts = Volts12(input4)
-  print("Input 4: {} ({} Bits) ({}V)".format(timestamp, input4,input4_volts))
+  input4voltsAvg = input4voltsAvg + input4_volts # Average all 3 entries
+  print("Input 4: {} ({} Bits) ({}V) ({}V)".format(timestamp, input4,input4_volts,input4voltsAvg))
+  if repeat == 1:
+    input4voltsAvg = input4voltsAvg / 3
+    input4voltsAvg = round(input4voltsAvg, 2) # Round off to 2 decimal places
+    print("\nInput 4: {} ({} Bits) ({}V) ({}V)".format(timestamp, input4,input4_volts,input4voltsAvg))
+    cht = open("/home/robin/CurrentAdc4Volts", "w")
+    cht.write (str(input4voltsAvg))
+    cht.close()
   input5 = analogInput(5) # Reading from CH5
   input5_volts = Volts12(input5)
-  print("Input 5: {} ({} Bits) ({}V)".format(timestamp, input5,input5_volts))
+  input5voltsAvg = input5voltsAvg + input5_volts # Average all 3 entries
+  print("Input 5: {} ({} Bits) ({}V) ({}V)".format(timestamp, input5,input5_volts,input5voltsAvg))
+  if repeat == 1:
+    input5voltsAvg = input5voltsAvg / 3
+    input5voltsAvg = round(input5voltsAvg, 2) # Round off to 2 decimal places
+    print("\nInput 5: {} ({} Bits) ({}V) ({}V)".format(timestamp, input5,input5_volts,input5voltsAvg))
+    cht = open("/home/robin/CurrentAdc5Volts", "w")
+    cht.write (str(input5voltsAvg))
+    cht.close()
   sleep(sleepTime)
   repeat = repeat - 1
