f2a339e3324adfc411ee2a939325b6088ce9cb0c
Robin Greig <robin.greig@calalta.com>
2020-06-15 10:32:58 -0600

    Delete footer.php

--- dms/assets/footer.php
+++
@@ -1,6 +0,0 @@
-</div>
-<div id="footer">
-  <p>&copy; SAIT Polytechnic, School of ICT, Technical Services, Electronics shop / E119<br /><a href="mailto:ict.electronics@sait.ca">Contact us by email here!</a></p>
-</div>
-</body>
-</html>
