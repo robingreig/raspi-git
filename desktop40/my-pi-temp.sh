480d9b6f5e51a9385c2ba5e2c13da01a9ac11b58
Robin Greig <robin.greig@calalta.com>
2020-06-15 10:28:58 -0600

    Delete my-pi-temp.sh

--- desktop40/my-pi-temp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script: my-pi-temp.sh
-# Purpose: Display the ARM CPU and GPU temp of the Raspiberry Pi
-cpu=$(</sys/class/thermal/thermal_zone0/temp)
-echo "$(date) @ $(hostname)"
-echo "----------------------"
-echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)"
-echo "CPU => $((cpu/1000))'C"
-
