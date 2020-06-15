5540a4e7ec4cedbb2f792ba7054ba5276694e21e
Robin Greig <robin.greig@calalta.com>
2020-06-15 10:33:15 -0600

    Delete tablerow.php

--- dms/assets/tablerow.php
+++
@@ -1,7 +0,0 @@
-<tr>
-    <td class="date"><?php echo $row['date']; ?></td>
-    <td class="time"><?php echo $row['time']; ?></td>
-    <td class="dms" title="DMS temp."><?php echo $row['dms_temp']; ?></td>
-    <td class="e119" title="E119 temp."><?php echo $row['E119_temp']; ?></td>
-    <td class="e501" title="E501 temp."><?php echo $row['E501_temp']; ?></td>
-</tr>
