4dd240f7571df3f791a9ed2f0290bfb21108fb36
Robin <robin.greig@calalta.com>
2021-02-13 15:34:35 -0700

    updated garage34

--- garage34/camera02.py
+++ garage34/camera02.py
@@ -2,11 +2,15 @@
 
 from picamera import PiCamera
 from time import sleep
+import datetime
 
 camera = PiCamera()
 
 camera.start_preview()
 sleep(2)
+frame = 1
+
 for i in range(5):
-    camera.capture('/home/robin/Desktop/image%s.jpg' % i)
+    now = datetime.datetime.now()
+    camera.capture('/home/robin/Desktop/image,%s.jpg' % now)
 camera.stop_preview()
