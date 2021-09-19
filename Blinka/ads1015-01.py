6972d99c4f2dbe6e8878b13e4e3cfa48bb6841aa
Robin <robin.greig@calalta.com>
2021-09-19 11:34:45 -0600

    updated Blinks

---
+++ Blinka/ads1015-01.py
@@ -0,0 +1,11 @@
+import board
+import busio
+i2c = busio.I2C(board.SCL, board.SDA)
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+ads = ADS.ADS1015(i2c)
+
+chan = AnalogIn(ads, ADS.P0)
+print(chan.value, chan.voltage)
+
