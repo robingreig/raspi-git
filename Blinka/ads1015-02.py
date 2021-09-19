976764d3d7418faaa136874f3fa7134a41864faf
Robin <robin.greig@calalta.com>
2021-09-19 15:44:56 -0600

    updated ads1015

--- Blinka/ads1015-02.py
+++ Blinka/ads1015-02.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import time
 import board
 import busio
--- Blinka/ads1015-03.py
+++ Blinka/ads1015-03.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 import board
 import busio
+import os
 i2c = busio.I2C(board.SCL, board.SDA)
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
 ads = ADS.ADS1015(i2c)
 
-chan = AnalogIn(ads, ADS.P0)
-print(chan.value, chan.voltage)
-chan = AnalogIn(ads, ADS.P1)
-print(chan.value, chan.voltage)
-chan = AnalogIn(ads, ADS.P2)
-print(chan.value, chan.voltage)
 chan = AnalogIn(ads, ADS.P3)
 print(chan.value, chan.voltage)
-
+num = round(chan.voltage,3)
+print(num)
+cht = open("/home/robin/ads1015-1", "w")
+cht.write(str(num))
+cht.close()
