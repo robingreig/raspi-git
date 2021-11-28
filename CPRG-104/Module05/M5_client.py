ce4b24556c9224da3ddbc8b21be57e6a53e7ef40
Robin <robin.greig@calalta.com>
2021-11-27 21:50:18 -0700

    updated cprg-104

--- CPRG-104/Module05/M5_client.py
+++ CPRG-104/Module05/M5_client.py
@@ -9,12 +9,16 @@ import socket
 # Create a socket connection for client
 soc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 # Get the server IP and port
-ip = socket.gethostname()
+#ip = socket.gethostname()
+ip = '192.168.200.138'
 port = 5490
 
 # Try connecting to the server
 soc.connect((ip,port))
 
+
+# Prepare a cursor
+##cursor = dbc.cursor()
 # See what server sent
 message = soc.recv(1024)
 print(message.decode('utf-8'))
