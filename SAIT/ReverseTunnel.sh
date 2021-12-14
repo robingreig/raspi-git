ba29bd76890ffaa421693a2ab705bcf3909bfae9
Robin <robin.greig@calalta.com>
2021-12-13 20:26:25 -0700

    updated ReverseTunnel.sh

--- SAIT/ReverseTunnel.sh
+++ SAIT/ReverseTunnel.sh
@@ -4,4 +4,4 @@
 #autossh -f -N -T -R22201:localhost:22 raspi20.hopto.org
 #autossh -f -M 0 -N -T -o "ServerAliveInterval 120" -R22201:localhost:22 raspi20.hopto.org
 #autossh -f -M 0 -N -T -o "ServerAliveInterval 120" -o "ExitOnForwardFailure=yes" -R22201:localhost:22 raspi20.hopto.org
-autossh -f -M 0 -N -T -o "ServerAliveInterval 180" -o "ExitOnForwardFailure=yes" -R22221:localhost:22 raspi20.hopto.org
+autossh -f -M 0 -N -T -o "ServerAliveInterval 180" -o "ExitOnForwardFailure=yes" -o "ServerAliveCountMax 3" -R22211:localhost:22 raspi20.hopto.org
