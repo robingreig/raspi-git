0550ce48b703f67bfdaa1ce2e9f0dff74b649c95
Robin <robin.greig@calalta.com>
2021-12-18 08:03:58 -0700

    updated SMSDatabase

--- SMSDatabase/dailyBackup.py
+++ SMSDatabase/dailyBackup.py
@@ -6,7 +6,7 @@ def progress(status, remaining, total):
     print(f'Copied {total-remaining} of {total} pages...')
 
 con = sqlite3.connect('/home/robin/makerspace.db')
-bck = sqlite3.connect('/home/robin/makerspace_backup.`date +%F`.db')
+bck = sqlite3.connect('/home/robin/makerspace_backup.db')
 with bck:
     con.backup(bck, pages=3, progress=progress)
 bck.close()
