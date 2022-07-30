d6c0b1663356f6903a7db71602ca7a705f8289e5
Robin <robin.greig@calalta.com>
2022-07-30 13:30:17 -0600

    updated serial06 in Pico

---
+++ C/Pointers01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main (void)
+{
+	int a;
+	int *ptr_to_a;
+	
+	ptr_to_a = &a;
+	
+	a = 5;
+	printf("The value of a is %d\n", a);
+	
+	*ptr_to_a = 6;
+	printf("The value of a is %d\n", a);
+	
+	printf("The value of ptr_to_a is %d\n", ptr_to_a);
+	printf("It stores the value %d\n", *ptr_to_a);
+	printf("The address of a is %d\n", &a);	
+}
