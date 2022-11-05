eb9fc9151092a2a5edb3a5cc2edc75d103160f04
Robin <robin.greig@calalta.com>
2022-11-05 14:46:00 -0600

    updated zero11

---
+++ Zero11/Zero11_mqtt.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python3
+
+import paho.mqtt.client as mqtt # import the client 1
+import time
+import RPi.GPIO as GPIO
+
+##### Setup GPIO
+pinNum = 24
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM) # numbering scheme for breakout board
+GPIO.setup(pinNum,GPIO.OUT) # set pinNum as output
+GPIO.output(pinNum,GPIO.LOW) # set pinNum low to ensure fan off
+
+#####
+def on_message(client, userdata, message):
+    print("Message received ", str(message.payload.decode("utf-8")))
+    if str(message.payload.decode("utf-8")) == "true":
+      print("********** Light is off")
+      GPIO.output(pinNum,GPIO.LOW) # turn GPIO Off & Fan Off
+    if str(message.payload.decode("utf-8")) == "false":
+      print("********** Light is on")
+      GPIO.output(pinNum,GPIO.HIGH) # turn GPIO On & Fan On
+    print("Message topic = ", message.topic)
+    print("Message qos = ", message.qos)
+    print("Message retain flag = ", message.retain)
+#####
+
+def on_log(client, userdata, level, buf):
+    print("log: ",buf)
+#####
+
+broker_address = "192.168.200.21"
+print("Creating new instance")
+client = mqtt.Client("Zero11") # create a new instance
+client.on_log=on_log
+client.on_message=on_message # attach function to callback
+print("Connecting to broker")
+client.connect(broker_address) # connect to broker
+#client.loop_start() # start the loop
+print("Subscribing to topic, Irricana/OutsideLight")
+client.subscribe("Irricana/OutsideLight", qos = 2)
+time.sleep(1)
+#client.loop_stop() # stop the loop
+client.loop_forever() # start the loop
