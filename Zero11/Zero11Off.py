d0ddc73fcf433586c59e97e8d04a58ac2810bc88
Robin <robin.greig@calalta.com>
2022-11-05 20:51:50 -0600

    updated Zero11

---
+++ Zero11/Zero11Off.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#====================================================================
+# Import subfiles
+#====================================================================
+import sys
+import os
+import RPi.GPIO as GPIO
+import warnings
+
+#====================================================================
+# Set Variables
+#====================================================================
+pinNum = 24
+GPIO.setwarnings(False) # Don't display the warnings
+GPIO.setmode(GPIO.BCM) # Numbering scheme that corresponds to breakout board and pin layout
+GPIO.setup(pinNum,GPIO.OUT) # Replace pinNum with whatever pin you used, this sets up that pin as an output
+
+GPIO.output(pinNum,GPIO.LOW) # Turn the Outside Lights OFF
