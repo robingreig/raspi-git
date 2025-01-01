06e4fa04b55eb85db0a6c84e9e5a0d796e61189c
Robin <robin.greig@calalta.com>
2024-12-31 19:43:45 -0700

    updated garage34

---
+++ garage34/movePics01.sh
@@ -0,0 +1,13 @@
+
+
+
+<<<<<<< HEAD
+scp /home/robin/PicsTemp/*.jpg robin@192.168.200.24:/home/robin/Pictures/
+#scp /home/robin/PicsTemp/*.jpg robin@192.168.200.150:/home/robin/Pictures/
+mv /home/robin/PicsTemp/*.jpg /home/robin/Pictures/
+=======
+scp /home/robin/PicsTemp/* robin@192.168.200.24:/home/robin/Pictures/
+scp /home/robin/PicsTemp/* robin@192.168.200.150:/home/robin/Pictures/
+mv /home/robin/PicsTemp/* /home/robin/Pictures/
+
+>>>>>>> 11427437e7ce8d9b3cb0f3bfc4dfac5251bb70cf
