806ab6afcacaf289d787e87c370c9add8eed45d3
Robin <robin.greig@calalta.com>
2025-01-05 11:19:49 -0700

    updated weather

--- Wunderground/WUupload.py
+++ Wunderground/WUupload.py
@@ -1,6 +1,9 @@
 import requests
 
-ambient_temp=-15.83
+ambient_tempc=-15.83
+print("ambient_tempc: ",ambient_tempc)
+ambient_tempf = (ambient_tempc *1.8)+32
+print("ambient_tempf: ",ambient_tempf)
 
 # create a string to hold the first part of the URL
 
@@ -11,13 +14,22 @@ WUcreds = "ID=" + WU_station_id + "&PASSWORD="+ WU_station_pwd
 date_str = "&dateutc=now"
 action_str = "&action=updateraw"
 
-temp_str = "{0:.2f}",format(ambient_temp)
+temp_str = "{0:.2f}".format(ambient_tempf)
+print("temp_str: ",temp_str)
 
-r= requests.get(
-    WUurl +
-    WUcreds +
-    date_str +
-    "&tempf=" + temp_str +
-    action_str)
+r = requests.get(
+	str(WUurl) +
+	str(WUcreds) +
+	str(date_str) +
+	"&tempf=" + str(temp_str) +
+	str(action_str))
+
+#r= requests.get(
+#    WUurl + 
+#    WUcreds + 
+#    date_str + 
+#    "&tempf=" + temp_str + 
+#    action_str)
+
 
 print("Received " + str(r.status_code) + " " + str(r.text))
