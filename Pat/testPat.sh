5fcd1a10cc79194ed82fa8a169c8d91d45bfb45a
Robin Greig <robin.greig@calalta.com>
2025-11-16 11:43:10 -0700

    updated pat

--- Pat/testPat.sh
+++ Pat/testPat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Sending text from bash" | pat compose VE6RBN -s BashTest robin.greig@sait.ca
+echo "Sending text from bash" | pat compose VE6RBN -s BashTest robin.greig@calalta.com
 pat connect telnet
---
+++ Pat/testPatDesktop500.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Sending text from Desktop500" | pat compose VE6RBN -s BashTest robin.greig@calalta.com
+pat connect telnet
