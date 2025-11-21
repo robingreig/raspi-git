6798dc7bc5426cd1f5f1ae3c373607507b7bb123
Robin <robin.greig@calalta.com>
2025-11-21 13:56:19 -0700

    added camera01

---
+++ picamera2/camera01.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+##########
+# Author: Robin Greig
+# Filename: camera01.py
+# 2025.11.21
+# 1) If the mandoor is opened
+# 2) Turn on the LED light
+# 3) Take 10 pics (Num_Pics)
+##########
+
+from picamera2 import Picamera2
+from time import sleep
+import datetime
+from gpiozero import Button, LED
+import os
+
+# Assign variable 'camera' to the Pi Camera
+picam2 = Picamera2()
+#config = picam2.create_still_configuration()
+#picam2.configure(config)
+# Assign variable 'door' as an input (Button) on GPIO23
+door = Button(23)
+# Assign variable 'light' as an output (LED) on GPIO25
+light = LED(25)
+# Relay for light is off when output is High (on)
+light.on()
+# Debug = 0 to stop test messages, Debug = 1 to print
+Debug = 1
+# Number of pics to be taken each time door is opened
+Num_Pics = 10
+# Delay between pics taken
+Camera_Sleep = 1
+# Delay x 60 before additional sets of pics taken if door isn't closed
+Picture_Cycle = 5
+
+sleep(Camera_Sleep)
+
+while True:
+    try:
+        door.wait_for_release() # Door opened
+        if Debug > 0:
+            print("Door Opened!")
+        light.off() # output low = Light ON
+        for i in range(Num_Pics):
+            now = datetime.datetime.now()
+            picam2.start_and_capture_file('/home/robin/PicsTemp/%s.jpg' % now)
+#        Send a copy of the pics to battmon24
+#        os.system("/home/robin/raspi-git/garage34/movePics.sh")
+#       Send email that door is opened
+#        os.system("/home/robin/raspi-git/Python3/SMTP/Garage34Door.py")
+        for j in range(60):
+            if door.is_pressed:
+                light.on() # output high = Light OFF
+                if Debug > 0:
+                    print("Door is closed")
+                break
+            if Debug > 0:
+                print("Waiting for door to close: %s" %j)
+            sleep(Picture_Cycle)
+    except KeyboardInterrupt:
+        break
