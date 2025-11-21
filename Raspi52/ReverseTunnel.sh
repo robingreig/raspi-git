9a349ad0d8f45bfaccebd5d9cb9208958bebe29d
Robin <robin.greig@calalta.com>
2025-11-21 15:00:12 -0700

    updated raspi52

--- Raspi52/ReverseTunnel.sh
+++ Raspi52/ReverseTunnel.sh
@@ -4,4 +4,6 @@
 #autossh -f -N -T -R22201:localhost:22 raspi20.hopto.org
 #autossh -f -M 0 -N -T -o "ServerAliveInterval 120" -R22201:localhost:22 raspi20.hopto.org
 #autossh -f -M 0 -N -T -o "ServerAliveInterval 120" -o "ExitOnForwardFailure=yes" -R22201:localhost:22 raspi20.hopto.org
+
 autossh -f -M 0 -N -T -o "ServerAliveInterval 180" -o "ExitOnForwardFailure=yes" -R22252:localhost:22 raspi20.hopto.org
+#autossh -f -M 0 -N -T -o "ServerAliveInterval 180" -o "ExitOnForwardFailure=yes" -R22252:localhost:22 192.168.200.20
