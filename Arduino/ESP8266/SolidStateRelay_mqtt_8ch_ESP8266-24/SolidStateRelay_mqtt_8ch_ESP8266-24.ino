f8e986ecad4a863e28fa4432f4b9e099de7adca5
Robin Greig <robin.greig@calalta.com>
2025-12-14 15:42:09 -0700

    updated SSR_mqtt_esp8266-24

---
+++ Arduino/ESP8266/SolidStateRelay_mqtt_8ch_ESP8266-24/SolidStateRelay_mqtt_8ch_ESP8266-24.ino
@@ -0,0 +1,166 @@
+
+/*
+  SolidStateRelay_mqtt_8ch_ESP8266-24
+  Robin Greig, 2025.12.12
+  Use 8 channel Solid Stare Relay board with ESP8266 MCU on terminal block
+  mqtt keepalive is only 15 seconds so I've added 2 functions
+  to check for wifi & mqtt connection at the start of the loop
+  mqtt broker: mqtt43.local
+  mqtt topic: esp8266/24/GPIO
+  Send mqtt 00000000 for all off & 11111111 for all on
+  Each digit represents a relay (first on left & eigth on right)
+*/
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Calalta02"; // Enter your WiFi name
+const char* password =  "Micr0s0ft2018"; // Enter WiFi password
+const char* mqttServer = "192.168.200.143";
+const int mqttPort = 1883;
+const char* mqttUser = "otfxknod";
+const char* mqttPassword = "nSuUc1dDLygF";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+int pinNum[8] = {16, 05, 04, 00, 14, 12, 13, 15};
+// Initialize Relay 1, GPIO16, D0, WAKE pin as an output
+// Initialize Relay 2, GPIO05, D1, SCL pin as an output
+// Initialize Relay 3, GPIO04, D2, SDA pin as an output
+// Initialize Relay 4, GPIO00, D3, FLASH pin as an output
+// Initialize Relay 5, GPIO14, D5, SCLK pin as an output
+// Initialize Relay 6, GPIO12, D6, MISO pin as an output
+// Initialize Relay 7, GPIO13, D7, MOSI pin as an output
+// Initialize Relay 8, GPIO15, D8, CS pin as an output
+
+const char *rssi = "esp8266/24/RSSI"; // MQTT publish topic for signal RSSI
+
+void reconnectMQTT() {
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    // Unique client ID (using ESP8266 macAddress)  
+    String client_id = "esp8266-";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s is connecting to the mqtt broker\n", client_id.c_str()); 
+//    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+    if (client.connect(client_id.c_str())) {
+      Serial.printf("The client %s is connected to MQTT\n", client_id.c_str());
+      String WiFiRSSI = String(WiFi.RSSI());
+      client.publish(rssi,WiFiRSSI.c_str(),"-r"); // retain flag set
+      Serial.printf("The client RSSI is %s\n",WiFiRSSI.c_str());
+      client.subscribe("esp8266/24/GPIO"); // subscribe to mqtt channel
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(500);
+    }
+  }
+}
+
+void reconnectWiFi() {
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+ 
+  Serial.begin(115200);
+  for (int i = 0; i < 9; i++){
+    pinMode(pinNum[i], OUTPUT);
+    digitalWrite(pinNum[i], HIGH); 
+  }
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+}
+ 
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Received ansi: ");
+  Serial.println(*payload);
+  Serial.print("Full Message Arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  // Solid State Relays turn on with a 0 sent and off with a 1 sent
+  // Switch on Relay 1 & the BuiltInLED if a 0 was received as the first character
+  if ((char)payload[0] == '0') {
+    digitalWrite(pinNum[0], HIGH);   // Turn on Relay 1
+    digitalWrite(BUILTIN_LED, LOW); // Turn on BuiltIn LED
+  } else {
+    digitalWrite(pinNum[0], LOW);  // Turn off Relay 1
+    digitalWrite(BUILTIN_LED, HIGH); // Turn off BuiltIn LED
+  }
+  // Switch on Relay 2 if a 1 was received as the second character
+  if ((char)payload[1] == '0') {
+    digitalWrite(pinNum[1], HIGH);   // Turn on Relay 2
+  } else {
+    digitalWrite(pinNum[1], LOW);  // Turn off Relay 2
+  }
+  // Switch on Relay 3 if a 1 was received as the third character
+  if ((char)payload[2] == '0') {
+    digitalWrite(pinNum[2], HIGH);   // Turn on Relay 3
+  } else {
+    digitalWrite(pinNum[2], LOW);  // Turn off Relay 3
+  }
+  // Switch on Relay 4 if a 1 was received as the fourth character
+  if ((char)payload[3] == '0') {
+    digitalWrite(pinNum[3], HIGH);   // Turn on Relay 4
+  } else {
+    digitalWrite(pinNum[3], LOW);  // Turn off Relay 4
+  }
+  // Switch on Relay 5 if a 1 was received as the fifth character
+  if ((char)payload[4] == '0') {
+    digitalWrite(pinNum[4], HIGH);   // Turn on Relay 5
+  } else {
+    digitalWrite(pinNum[4], LOW);  // Turn off Relay 5
+  }
+  // Switch on Relay 6 if a 1 was received as the sixth character
+  if ((char)payload[5] == '0') {
+    digitalWrite(pinNum[5], HIGH);   // Turn on Relay 6
+  } else {
+    digitalWrite(pinNum[5], LOW);  // Turn off Relay 6
+  }
+  // Switch on Relay 7 if a 1 was received as the seventh character
+  if ((char)payload[6] == '0') {
+    digitalWrite(pinNum[6], HIGH);   // Turn on Relay 7
+  } else {
+    digitalWrite(pinNum[6], LOW);  // Turn off Relay 7
+  }
+  // Switch on Relay 8 if a 1 was received as the eighth character
+  if ((char)payload[7] == '0') {
+    digitalWrite(pinNum[7], HIGH);   // Turn on Relay 8
+  } else {
+    digitalWrite(pinNum[7], LOW);  // Turn off Relay 8
+  }
+  String WiFiRSSI = String(WiFi.RSSI());
+  // publish RSSI to esp/rssi with retain flag set
+  client.publish("esp/rssi",WiFiRSSI.c_str(),"-r");
+  delay(1000);
+}
+  
+void loop()
+{
+  // Connect to WiFi if not connected
+  if (WiFi.status() != WL_CONNECTED) {
+    reconnectWiFi();
+  }
+
+  // Connect to MQTT if not connected
+  if(!client.connected()) {
+    reconnectMQTT();
+  }
+
+  client.loop();
+}
