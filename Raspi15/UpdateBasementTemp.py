b233188d787e02701937344980af6f88f31d5cb8
Robin <robin.greig@calalta.com>
2026-01-10 13:55:19 -0700

    updated raspi15

--- Raspi15/UpdateBasementTemp.py
+++ Raspi15/UpdateBasementTemp.py
@@ -11,45 +11,50 @@ import paho.mqtt.client as mqtt #import the client1
 DEBUG = 0
 
 def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+    if DEBUG > 0:
+      print("message received " ,str(message.payload.decode("utf-8")))
+    global BasementTemp
+    BasementTemp = str(message.payload.decode("utf-8"))
+    if DEBUG > 0:
+      print ("Basement Temp sent to aio: ", BasementTemp)
+      print("message topic=",message.topic)
+      print("message qos=",message.qos)
+      print("message retain flag=",message.retain)
 
 while(True):
 
-  broker_address="192.168.1.143"
-
-  print("creating new instance")
+  broker_address="192.168.200.143"
+  if DEBUG > 0:
+    print("creating new instance")
   client = mqtt.Client("Rpi15") #create new instance
   client.on_message=on_message #attach function to callback
-  print("connecting to broker")
+  if DEBUG > 0:
+    print("connecting to broker")
   client.connect(broker_address) #connect to broker
   client.loop_start() #start the loop
-  print("Subscribing to topic","esp8266/18/temp")
+  if DEBUG > 0:
+    print("Subscribing to topic","esp8266/18/temp")
   client.subscribe("esp8266/18/temp")
 #   print("Publishing message to topic","house/bulbs/bulb1")
 #   client.publish("house/bulbs/bulb1","OFF")
   time.sleep(4) # wait
   client.loop_stop() #stop the loop
 
+  if DEBUG > 0:
+    print ("Basement Temp outside of function: ", BasementTemp)
 
-#  OutsideTemp = (round(read_temp3(),1))
-#  if DEBUG > 0:
-#    print ("Outside Temp sent to aio: ", OutsideTemp)
+  cht = open("/home/robin/CurrentBasementTemp", "w")
+  cht.write (str(BasementTemp))
+  cht.close()
 
-#  cht = open("/home/robin/CurrentOutsideTemp", "w")
-#  cht.write (str(OutsideTemp))
-#  cht.close()
-  
-#  aio = Client('robingreig', 'd0c57dc7661d4b2e8a1868133f9e162c')
-#  aio.send('outside-temp', OutsideTemp)
-# Retrieve the most recent value from the feed 'Foo'.
+  aio = Client('robingreig', 'd0c57dc7661d4b2e8a1868133f9e162c')
+  aio.send('basement-temp', BasementTemp)
+# Retrieve the most recent value from the feed 'basement-temp'
 # Access the value by reading the `value` property on the returned Data object.
 # Note that all values retrieved from IO are strings so you might need to convert
 # them to an int or numeric type if you expect a number.
-#  if DEBUG > 0:
-#    data = aio.receive('outside-temp')
-#    print('Received value: {0}'.format(data.value))
+  if DEBUG > 0:
+    data = aio.receive('basement-temp')
+    print('Received value: {0}'.format(data.value))
 
   break
--- Raspi15/UpdateTemps.sh
+++ Raspi15/UpdateTemps.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
 /home/robin/raspi-git/Raspi15/UpdateAtticTemp.py
+echo -n "CurrentAtticTemp: "
 cat /home/robin/CurrentAtticTemp
-echo " > CurrentAtticTemp: "
+echo ""
 
 /home/robin/raspi-git/Raspi15/UpdateCeilingTemp.py
+echo -n "CurrentCeilingTemp: "
 cat /home/robin/CurrentCeilingTemp
-echo " > CurrentCeilingTemp: "
+echo ""
+
 /home/robin/raspi-git/Raspi15/UpdateDeskTemp.py
+echo -n "CurrentDeskTemp: "
 cat /home/robin/CurrentGarageTemp
-echo " > CurrentDeskTemp: "
+echo ""
+
 /home/robin/raspi-git/Raspi15/UpdateOutsideTemp.py
+echo -n "CurrentOutsideTemp: "
 cat /home/robin/CurrentOutsideTemp
-echo " > CurrentOutsideTemp: "
+echo ""
+
+/home/robin/raspi-git/Raspi15/UpdateBasementTemp.py
+echo -n "CurrentBasementTemp: "
+cat /home/robin/CurrentBasementTemp
+echo ""
+
 /home/robin/raspi-git/Raspi15/allTempPub01.py
 
 
