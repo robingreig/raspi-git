c3fe959280ccd5596b0fcc483c96777bd74b1213
Robin <robin.greig@calalta.com>
2026-04-19 16:09:47 -0600

    updated trainAudio02.py

--- trainAudio32/loadAsoundrc.sh
+++ trainAudio32/loadAsoundrc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sleep 15
+sleep 10s
 
 #cp /home/robin/raspi-git/trainAudio32/asoundrc /home/robin/.asoundrc
-aplay /home/robin/raspi-git/trainAudio32/TrainHorn01.wav
+#aplay /home/robin/raspi-git/trainAudio32/TrainHorn01.wav
+/home/robin/raspi-git/trainAudio32/trainAudio02.py
--- trainAudio32/trainAudio02.py
+++ trainAudio32/trainAudio02.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 
+# trainAudio02.py
+# Robin Greig for Pioneer Acres
+# 2026.04.19
+# Hall Effect device will trigger the audio track when the train engine
+# passes over top near the car crossing 
+
 import time
 import os
 from gpiozero import Button
 
-button = Button(2) # set GPIO2 as input trigger for audio
+# set GPIO 02 as input trigger for hall effect device
+button = Button(2)
 
-# if debug > 0 then outputs will be turned OFF
+# if debug > 0 then printed outputs will be turned OFF
 debug = 0
 
 while True:
     try:
-        # button.when_released  = os.system('aplay /home/robin/raspi-git/trainAudio32/TrainHorn01.wav')
-        button.wait_for_release()
+        # button.when_released()
+        # button.wait_for_release()
+        button.wait_for_press()
         os.system('aplay /home/robin/raspi-git/trainAudio32/TrainHorn01.wav')
         if debug > 0:
             print('Train Detected')
